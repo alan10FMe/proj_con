9430ec56b0574a00349d1ac0ea5dc2749508ef4b
alan10FMe <alan10fm@gmail.com>
2016-02-08 21:29:55 -0800

    Changes for theme for the app

--- app/src/main/java/com/nojsoft/conquian/BoardActivity.java
+++ app/src/main/java/com/nojsoft/conquian/BoardActivity.java
@@ -7,12 +7,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends AppCompatActivity {
+public class BoardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_board);
     }
 
 }
