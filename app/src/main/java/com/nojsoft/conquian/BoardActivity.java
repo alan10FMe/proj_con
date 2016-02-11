48bc39d58b69e4f2d72ff172a45617880797c42e
alan10FMe <alan10fm@gmail.com>
2016-02-10 21:19:01 -0800

    Changes to divide screen for each players hand and game

--- app/src/main/java/com/nojsoft/conquian/BoardActivity.java
+++ app/src/main/java/com/nojsoft/conquian/BoardActivity.java
@@ -5,14 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 
 public class BoardActivity extends AppCompatActivity {
+
+    private ImageButton btnDeck;
+    private ImageView imgCardPlaying;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        btnDeck = (ImageButton)findViewById(R.id.btn_deck);
+        imgCardPlaying = (ImageView)findViewById(R.id.img_card_playing);
     }
 
 }
