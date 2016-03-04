4effbe43cf717822d6229e00dfc504d2dabac0a1
Bi0R <bior87@gmail.com>
2016-03-03 22:57:05 -0600

    Updated to handle CardView instead of Card

--- app/src/main/java/com/nojsoft/conquian/bean/Hand.java
+++ app/src/main/java/com/nojsoft/conquian/bean/Hand.java
@@ -15,7 +15,7 @@ import java.util.List;
  * Created by jorge on 11/02/16.
  */
 public class Hand {
-    private Card [] cards;//Array with cards, the player will get 8, the 9th card is the objective
+    private CardView [] cards;//Array with cards, the player will get 8, the 9th card is the objective
     private CardView[] imageCards;//Array with the image representation in the UI for each card
     private Context context;
     private int id;
@@ -41,7 +41,7 @@ public class Hand {
      *
      * @param cards Array of Card objects, the cards the user has in his/her hand
      */
-    public  void set (Card [] cards ){
+    public  void set (CardView [] cards ){
         this.cards = cards;
     }
 
@@ -50,7 +50,7 @@ public class Hand {
      *
      * @return Array of Card objects, the cards the user has in his/her hand
      */
-    public Card[] getCards (){
+    public CardView[] getCards (){
         return this.cards;
     }
 
@@ -78,13 +78,13 @@ public class Hand {
      *                  unique it can be used as identifier.
      * @return null if the size of the position's list is 0 or no coincidences were found
      */
-    public Card [] dropCard (int [] ids){
-        Card [] droppedCards = null;
+    public CardView [] dropCard (int [] ids){
+        CardView [] droppedCards = null;
         int position = 0;
         if (ids!= null && ids.length>0) {
-            droppedCards = new Card[ids.length];
+            droppedCards = new CardView[ids.length];
             for (int id : ids) {
-                for (Card card : cards) {
+                for (CardView card : cards) {
                     if (card.getId() == id) {
                         droppedCards[position++]=card;
                     }
@@ -92,7 +92,7 @@ public class Hand {
             }
 
             if(position>0){
-                for(Card dropped: droppedCards){
+                for(CardView dropped: droppedCards){
                     for(int i = 0; i< cards.length; i++){
                         if(cards[i]==dropped)
                             cards[i]=null;
@@ -112,7 +112,7 @@ public class Hand {
      */
     public void changePosition (int currentPos, int futurePos){
         boolean changed =false;
-        Card holder = cards[currentPos];
+        CardView holder = cards[currentPos];
         cards[currentPos] = cards[futurePos];
         cards[futurePos] = holder;
     }
@@ -124,7 +124,7 @@ public class Hand {
      */
     public int getHandPosition (int id){
         int position = -1;
-        Card card;
+        CardView card;
         for(int i = 0; i < cards.length; i++){
             card = cards [i];
             if(card.getId() == id)
