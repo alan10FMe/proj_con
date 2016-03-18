70d052c86c5068c895ff754992794ebe72e3c272
Bi0R <bior87@gmail.com>
2016-03-17 20:50:51 -0600

    Refactor validateGroup method

--- app/src/main/java/com/nojsoft/conquian/bean/GameValidator.java
+++ app/src/main/java/com/nojsoft/conquian/bean/GameValidator.java
@@ -15,48 +15,38 @@ public class GameValidator {
 
     public static boolean validateGroup (GroupView group){
         boolean isValid = false;
-        String flag = "sn";
         CardView currentCard;
         CardView previousCard;
+
         if (group.getChildCount() <= 1){
             group.setGameType(CardConstants.GameType.NONE);
             return true;//first card so it's correct no matter what
         }
 
-        if (group.getChildCount() == 2){
-            //Define type of group and set it to the flag
-            currentCard = (CardView) group.getChildAt(1);
-            previousCard = (CardView) group.getChildAt(0);
+        if (group.getChildCount() >= 2){
+            currentCard = (CardView) group.getChildAt(group.getChildCount()-1);
+            previousCard = (CardView) group.getChildAt(group.getChildCount()-2);
             if (currentCard.getNumValue() == previousCard.getNumValue()) {
-                group.setGameType(CardConstants.GameType.SAME_KIND);
-                return true;
+                if ( group.getGameType() == CardConstants.GameType.NONE) {
+                    group.setGameType(CardConstants.GameType.SAME_KIND);
+                    return true;
+                }
+                if ( group.getGameType() == CardConstants.GameType.SAME_KIND ){
+                    return true;
+                }
             }
             if (currentCard.getType().equals(previousCard.getType())) {
                 if ( Math.abs( currentCard.getNumValue() - currentCard.getNumValue()) == 1) {
-                    group.setGameType(CardConstants.GameType.STRAIGHT);
-                    return true;
-                }
-            }
-        }
-
-        if (group.getChildCount() > 2) {
-            currentCard = (CardView) group.getChildAt(group.getChildCount()-1);
-            previousCard = (CardView) group.getChildAt(group.getChildCount()-2);
-            if (flag.equals(group.getGameType() == CardConstants.GameType.SAME_KIND)){
-                if (currentCard.getNumValue() == previousCard.getNumValue()) {
-                    // Set cards as one group
-                    return true;
-                }
-            } else {
-                if (currentCard.getType().equals(previousCard.getType())) {
-                    if ( Math.abs( currentCard.getNumValue() - currentCard.getNumValue()) == 1) {
-                        // set cards as same group
+                    if ( group.getGameType() == CardConstants.GameType.NONE) {
+                        group.setGameType(CardConstants.GameType.STRAIGHT);
+                        return true;
+                    }
+                    if ( group.getGameType() == CardConstants.GameType.STRAIGHT ){
                         return true;
                     }
                 }
             }
         }
-
         return isValid;
     }
 
